d77797ff4789167d631c8ab44e986f6b49345105
Colby Corcoran <colbyc.uark@gmail.com>
2025-06-20 10:33:21 -0500

    Initial Commit

---
+++ Christian Life JournalTests/Christian_Life_JournalTests.swift
@@ -0,0 +1,17 @@
+//
+//  Christian_Life_JournalTests.swift
+//  Christian Life JournalTests
+//
+//  Created by Colby Corcoran on 6/20/25.
+//
+
+import Testing
+@testable import Christian_Life_Journal
+
+struct Christian_Life_JournalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
